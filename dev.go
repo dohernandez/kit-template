17bf74a7a9ca99864b43bc3119c06472be480558
Darien Hernandez <dohernandez@gmail.com>
2024-12-07 16:06:15 +0100

    Patch missing noprune github.com/dohernandez/dev-grpc

--- dev.go
+++ dev.go
@@ -5,5 +5,6 @@ package noprune
 
 import (
 	_ "github.com/bool64/dev"                  // Include CI/Dev scripts to project.
-	_ "github.com/dohernandez/go-grpc-service" // Include development dev helpers to project.
+	_ "github.com/dohernandez/dev-grpc"        // Include development grpc helpers to project.
+	
 )
